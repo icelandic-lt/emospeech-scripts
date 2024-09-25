0dc85cdfbb086247e00dadc7a66c7acd8f4d4f2e
Gunnar Thor Örnólfsson <ornolfsson@gmail.com>
2024-09-25 10:35:48 +0000

    Add information about MFA to README

--- alignment/docker_data/run_entire_pipeline.sh
+++ alignment/docker_data/run_entire_pipeline.sh
@@ -8,4 +8,4 @@ bash scripts/prepare_alignment.sh input/ output/arranged_corpus
 mfa train --phone_groups_path scripts/alignment_data/phone_groups.yaml --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j12 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am2
 
 ## Now align the data
-mfa align --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j8 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am.zip output/alignment
+mfa align --phone_groups_path scripts/alignment_data/phone_groups.yaml --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j8 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am.zip output/alignment
