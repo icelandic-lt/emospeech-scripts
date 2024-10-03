8c4bb62c6061ff9ce053c43fe9fa9dffba08108d
Gunnar Thor Örnólfsson <ornolfsson@gmail.com>
2024-10-03 18:42:34 +0000

    fix symlink paths in the flattened version of the corpus

--- alignment/docker_data/run_entire_pipeline.sh
+++ alignment/docker_data/run_entire_pipeline.sh
@@ -2,7 +2,7 @@
 
 bash ./train_g2p.sh
 cd ~/data
-bash scripts/prepare_alignment.sh input/ output/arranged_corpus
+bash scripts/prepare_alignment.sh /home/mfauser/data/input/ /home/mfauser/data/output/arranged_corpus
 
 ## Using only speaker ids for speaker identification
 mfa train --phone_groups_path scripts/alignment_data/phone_groups.yaml --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j12 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am
