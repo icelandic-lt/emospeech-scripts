8adf6d19a739027d3696f8d89c0acf05ec3f2550
Gunnar Thor <ornolfsson@gmail.com>
2024-10-06 13:40:39 +0000

    Minor fix to alignment script and insertion of missing prompts from script. (#2)
    * fix references to trained MFA acoustic model, change permissions on train_g2p.sh and add librosa dependency
    
    * add missing utts to placeholder intensity script
    
    * fix symlink paths in the flattened version of the corpus

--- alignment/docker_data/run_entire_pipeline.sh
+++ alignment/docker_data/run_entire_pipeline.sh
@@ -2,10 +2,12 @@
 
 bash ./train_g2p.sh
 cd ~/data
-bash scripts/prepare_alignment.sh input/ output/arranged_corpus
+bash scripts/prepare_alignment.sh /home/mfauser/data/input/ /home/mfauser/data/output/arranged_corpus
 
 ## Using only speaker ids for speaker identification
-mfa train --phone_groups_path scripts/alignment_data/phone_groups.yaml --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j12 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am2
+mfa train --phone_groups_path scripts/alignment_data/phone_groups.yaml --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j12 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am
 
 ## Now align the data
-mfa align --phone_groups_path scripts/alignment_data/phone_groups.yaml --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j8 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am.zip output/alignment
+mfa align --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j8 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am.zip output/alignment
+
+mfa align --output_format json --speaker_characters 3 --g2p_model_path output/models/g2p.zip -j8 output/arranged_corpus scripts/alignment_data/word2pron.txt output/models/am.zip output/alignment
